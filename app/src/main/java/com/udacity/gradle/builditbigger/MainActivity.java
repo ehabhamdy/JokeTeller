d49849f4dc752ee3c58de235dae689eadf54aa59
ehabhamdy <ehabhamdy2012@gmail.com>
2017-04-30 21:11:06 +0200

    Testing JokeProvider library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.ehab.jokeprovider.JokeTeller;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeTeller teller = new JokeTeller();
+        Toast.makeText(this, teller.tellJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
