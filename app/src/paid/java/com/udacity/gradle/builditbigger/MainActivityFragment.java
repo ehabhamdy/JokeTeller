807750fa7ee288a944d13bff5b128e78e5ac76cd
ehabhamdy <ehabhamdy2012@gmail.com>
2017-05-12 18:49:03 +0200

    Creating paid version with no Ads

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -6,9 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 /**
  * Created by ehabhamdy on 5/12/17.
  */
